a37e96627c5dbfaf38300a4926ed5022cd2507b6
Zhetao Jia <zhetao.jia@duke.edu>
2016-08-23 15:59:59 -0400

    Add isotropic asymmetric structure

--- Eigenfrequency Opt/CompFreq.m
+++ Eigenfrequency Opt/CompFreq.m
@@ -1,10 +1,10 @@
 function goal = CompFreq(a1,a2,a3,b1,b2)
 model = Labyrinthine3__EigOpt_setup;
-model.param.set('a1', num2str(a1));
-model.param.set('a2', num2str(a2));
-model.param.set('a3', num2str(a3));
-model.param.set('b1', num2str(b1));
-model.param.set('b2', num2str(b2));
+model.param.set('a1', num2str(a1/1000.0));
+model.param.set('a2', num2str(a2/1000.0));
+model.param.set('a3', num2str(a3/1000.0));
+model.param.set('b1', num2str(b1/1000.0));
+model.param.set('b2', num2str(b2/1000.0));
 %model.param.set('b3', num2str(b3));
 %model.mesh('mesh1').autoMeshSize(meshSize);
 
